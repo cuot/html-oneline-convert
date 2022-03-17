017c606babceced68299cee27de1e19f8ae94916
cuot <deoret35@gmail.com>
2022-03-18 01:45:11 +0900

    HTML 한줄 변환기

---
+++ src/component/ConvertToHTML.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "../css/ConvertToHTML.css";
+
+export default function ConvertToHTML() {
+  const [val, setVal] = useState("");
+
+  const click = (props) => {
+    props.target.value = "";
+  };
+
+  const changeValue = (props) => {
+    // 9 = '/t', 10 = '\n', 13 = 'r'
+    const v = props.target.value;
+    let tmp = "";
+    if (v === "") {
+      setVal("");
+      return;
+    }
+    for (let i = 0; v.length > i; i++) {
+      if (
+        v.charCodeAt(i) !== 9 ||
+        v.charCodeAt(i) !== 10 ||
+        v.charCodeAt(i) !== 13
+      ) {
+        tmp += v.charAt(i);
+        setVal(tmp);
+      }
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="main">
+        <fieldset className="fd_s_TextArea">
+          <legend>&nbsp;HTML&nbsp;</legend>
+          <textarea
+            onChange={changeValue}
+            onClick={click}
+            className="HTML_TextArea"></textarea>
+        </fieldset>
+        <fieldset className="fd_s_convertArea">
+          <legend>&nbsp;Convert&nbsp;</legend>
+          <textarea className="HTML_ConvertArea" value={val}></textarea>
+        </fieldset>
+      </div>
+    </div>
+  );
+}
---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/Header.css";
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div className="top_menu">Convert to one line of HTML</div>
+      </div>
+    </header>
+  );
+}
